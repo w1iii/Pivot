6476583861cd257ef1e14415cefc0fdb6434995e
wii <lomugdanglf.19@gmail.com>
2026-01-28 11:12:16 +0800

    added on the signup backend database

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,9 +1,14 @@
 
 import {NextRequest, NextResponse} from "next/server";
+import pool from '../../lib/db'
 
-export async function POST(req: NextRequest){
+export async function POST(req: NextRequest, res: NextResponse){
     const body = await req.json();
     const {firstname, lastname, address, email, contactno, password} = body;
+
+      if (!password || !email) {
+        return NextResponse.json({ error: "Please enter username, password, and email", status: 400 });
+      }
 
     const user = {
         firstname: firstname,
@@ -13,7 +18,17 @@ export async function POST(req: NextRequest){
         contactno: contactno,
         password: password,
     }
-    const response = NextResponse.json({ message: "Logged in", user})
 
-    return response
+    const checkDupQuery = `
+       SELECT id FROM users WHERE email = ${email} 
+    `;
+
+    const existingEmail = pool.query(checkDupQuery);
+    if (existingEmail.rows.length > 0) {
+      return NextResponse.json({ error: "Email already registered", statud: 409 });
+    }
+
+    const password_hash = await bcrypt.hash(password, 10);
+    // const response = NextResponse.json({ message: "Logged in", user})
+
 }
