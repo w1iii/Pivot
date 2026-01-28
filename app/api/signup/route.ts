94773d82f73115028bf9cffc0bf3439ec7ddb13c
wii <lomugdanglf.19@gmail.com>
2026-01-28 16:53:00 +0800

    error on signup route.tsx, fix try catch and queries.

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,6 +1,7 @@
 
 import {NextRequest, NextResponse} from "next/server";
 import pool from '../../lib/db'
+import bcrypt from 'bcrypt';
 
 export async function POST(req: NextRequest, res: NextResponse){
     const body = await req.json();
@@ -23,12 +24,35 @@ export async function POST(req: NextRequest, res: NextResponse){
        SELECT id FROM users WHERE email = ${email} 
     `;
 
-    const existingEmail = pool.query(checkDupQuery);
-    if (existingEmail.rows.length > 0) {
-      return NextResponse.json({ error: "Email already registered", statud: 409 });
-    }
+    // const existingEmail = pool.query(checkDupQuery);
+    // if (existingEmail.rows.length()> 1) {
+    //   return NextResponse.json({ error: "Email already registered", statud: 409 });
+    // }
 
     const password_hash = await bcrypt.hash(password, 10);
-    // const response = NextResponse.json({ message: "Logged in", user})
 
+    try{
+      const addQuery = `
+            INSERT INTO users(firstname, lastname, address, email, contactno, password, created_at)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING id, email, first_name, last_name;
+          `;
+
+      const newuser = pool.query(addQuery, [firstname, lastname, address, email, contactno, password])
+          
+      NextResponse.json({ 
+        message: 'User added successfully,',
+        user: newuser.rows[0]
+    })
+    }catch(e){
+      console.log(e)
+      NextResponse.json({ message: 'Insert user error', e})
+
+  }
 }
+
+
+  
+
+
+
