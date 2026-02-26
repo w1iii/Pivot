20661672bcd0fb20b85096a6dfa8ee98e59f8b3f
wii <lomugdanglf.19@gmail.com>
2026-02-26 18:40:47 +0800

    implemented update cached data when symbol is removed from the watchlist

--- app/api/watchlist/route.ts
+++ app/api/watchlist/route.ts
@@ -22,7 +22,7 @@ export async function GET() {
   const userId = await getUserId();
   if (!userId) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
-  const cacheKey = `watchlist:${userId}` ;
+  const cacheKey = `watchlist:${userId}`;
 
   try{
     const cachedData = await redis.get(cacheKey);
@@ -62,19 +62,46 @@ export async function POST(req: Request) {
   if (!userId) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
   const { symbol } = await req.json();
+  // upper case the symbol to avoid duplicate because symbols are uppercased
+  const upperSymbol = symbol.toUpperCase();
+  const cacheKey = `watchlist:${userId}`;
+
   try{
-    await pool.query(
+    const result = await pool.query(
       'INSERT INTO watchlist (user_id, symbol) VALUES ($1, $2) ON CONFLICT DO NOTHING',
       [userId, symbol.toUpperCase()]
     );
+    
+    // if duplicate return success directly
+    if (result.rowCount === 0) {
+      return NextResponse.json({ success: true });
+    }
+
+    const cached = await redis.get(cacheKey);
+    if (cached) {
+      const watchlist = JSON.parse(cached);
+
+      // check symbol if already exist on the cached data
+      const exists = watchlist.some(
+        (item: any) => item.symbol === upperSymbol
+      );
+
+      // if doesn't exist push the data on the watchlist cached db
+      if (!exists) {
+        watchlist.push({ symbol: upperSymbol });
+
+        await redis.set(cacheKey, JSON.stringify(watchlist), {
+          EX: DEFAULT_EXPIRATION,
+        });
+      }
+    }
 
     return NextResponse.json({ success: true });
 
   }catch(e){
     console.log(e)
-    return NextResponse.json({ e })
+    return NextResponse.json({ error: "Server Error" }, {status: 500})
   }
-
 }
 
 export async function DELETE(req: Request) {
@@ -82,9 +109,34 @@ export async function DELETE(req: Request) {
   if (!userId) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
   const { symbol } = await req.json();
-  await pool.query(
-    'DELETE FROM watchlist WHERE user_id = $1 AND symbol = $2',
-    [userId, symbol.toUpperCase()]
-  );
-  return NextResponse.json({ success: true });
+  const upperSymbol = symbol.toUpperCase();
+  const cacheKey = `watchlist:${userId}`;
+
+  try{
+    await pool.query(
+      'DELETE FROM watchlist WHERE user_id = $1 AND symbol = $2',
+      [userId, upperSymbol] 
+    );
+
+    const cached = await redis.get(cacheKey);
+
+    if (cached) {
+      const watchlist = JSON.parse(cached);
+
+      // check if symbol in the watchlist if true filter out symbol
+      const updated = watchlist.filter(
+        (item: any) => item.symbol !== upperSymbol
+      );
+      console.log("Removed data from Redis cache")
+
+      await redis.set(cacheKey, JSON.stringify(updated), {
+        EX: DEFAULT_EXPIRATION,
+      });
+    }
+
+    return NextResponse.json({ success: true });
+  }catch(e){
+    console.log(e)
+    return NextResponse.json({ error: "Server Error" }, {status: 500})
+  }
 }
